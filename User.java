69565b3acf81dd9f337659f84b8162b0ac49cd0f
Jordan Neff <JN1024482@wcupa.edu>
2024-06-01 14:20:28 -0400

    Initial class creations - no functionality yet

---
+++ User.java
@@ -0,0 +1,49 @@
+public class User  { 
+    private String firstName;
+    private String lastName;
+    private int age;
+    private int height; 
+    private int weight;
+
+    // maybe add userPass and username 
+    
+    public User(String firstName, String lastName, int age, int height, int weight) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public int getHeight() {
+        return height;
+    }
+    public void setHeight(int height) {
+        this.height = height;
+    }
+    public int getWeight() {
+        return weight;
+    }
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+}
